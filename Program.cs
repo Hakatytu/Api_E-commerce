741c79a72171dab3567a3a64234fc01df679e3d8
Hakatytu <102003382+Hakatytu@users.noreply.github.com>
2025-06-06 22:50:50 +0000

    Implementação do Authorization JWT

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 using StoreApi2.Data;
 using StoreApi2.User;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("V1", new OpenApiInfo()
+    {
+        Version = "V1",
+        Title = "Api.E-commerce",
+        Description = "Api do E-commerce"
+    });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "O cabeçalho de autorização JWT está usando o scheme Bearer /r/n/r/n Digite o 'Bearer' antes de colocar o Token"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 builder.Services.AddScoped<AppDbContext>();
 
 
